84265ef6bcb76c6013e231e2a1fc1b4ca77ce8f3
bhawana-web19 <bhawanachadda123@gmail.com>
2022-07-23 15:51:47 +0530

    done

---
+++ test.js
@@ -0,0 +1,6 @@
+import {navbar, changeBg} from "./navbar.js";
+document.querySelector("#navbar").innerHTML=navbar();
+window.addEventListener("scroll", changeBg);
+
+import {footer} from "./Footer.js";
+document.querySelector("#footer").innerHTML=footer();
