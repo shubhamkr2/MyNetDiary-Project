22ebf26dbb4b3118cc31342e5ab088625ce3924a
kartikkunwar <kunwarkartik@gmail.com>
2022-07-24 10:55:06 +0530

    done

--- plan.js
+++ plan.js
@@ -1,10 +1,10 @@
 // For Header_nav
-import { header_nav, MyFun } from "./Header.js";
-document.querySelector("#header").innerHTML=header_nav();
+// import { header_nav, MyFun } from "./Header.js";
+// document.querySelector("#header").innerHTML=header_nav();
 
-document.querySelector(".drop_btn").addEventListener("click",MyFun);
-import { FooterLast } from "./FooterLast.js";
-document.querySelector("#footer").innerHTML=FooterLast();
+// document.querySelector(".drop_btn").addEventListener("click",MyFun);
+// import { FooterLast } from "./FooterLast.js";
+// document.querySelector("#footer").innerHTML=FooterLast();
 
 
 let count=0;
@@ -17,4 +17,21 @@ document.getElementById("sliding").addEventListener("click",function(){
     }else{
         x.style.display="block"
     }
-})
+});
+
+function MyFun() {
+    document.getElementById("mydropdownlList").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.drop_btn')) {
+        var dropdownlLists = document.getElementsByClassName("dropdownlList-content");
+        var i;
+        for (i = 0; i < dropdownlLists.length; i++) {
+            var opendropdownlList = dropdownlLists[i];
+            if (opendropdownlList.classList.contains('show')) {
+                opendropdownlList.classList.remove('show');
+            }
+        }
+    }
+}
